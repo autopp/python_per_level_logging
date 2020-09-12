c04de0786264061c047f9379260987021a06fa64
autopp <autopp.inc@gmail.com>
2020-09-12 13:52:40 +0900

    feat: add default handler paramenter

--- per_level_logging/handler.py
+++ per_level_logging/handler.py
@@ -1,16 +1,21 @@
 from logging import Handler, LogRecord, NOTSET
-from typing import Dict
+from typing import Dict, Optional
 
 class PerLevelHandler(Handler):
-    def __init__(self, handlers: Dict[int, Handler]):
+    def __init__(self, handlers: Dict[int, Handler], default: Optional[Handler] = None):
         Handler.__init__(self, NOTSET)
         self.handlers = handlers
+        self.default = default
 
     def emit(self, record: LogRecord):
         handler = self.handlers.get(record.levelno, None)
         if handler is not None:
             handler.emit(record)
+        elif self.default is not None:
+            self.default.emit(record)
 
     def flush(self):
         for handler in self.handlers.values():
             handler.flush()
+        if self.default is not None:
+            self.default.flush()
--- tests/test_handler.py
+++ tests/test_handler.py
@@ -9,35 +9,56 @@ class TestPerLevelHandler(unittest.TestCase):
     def setUp(self):
         self.info_warn_buf = StringIO()
         self.err_buf = StringIO()
-        self.handler = PerLevelHandler({
+        self.default_buf = StringIO()
+
+        self.handlers = {
             logging.INFO: logging.StreamHandler(self.info_warn_buf),
             logging.WARNING: logging.StreamHandler(self.info_warn_buf),
             logging.ERROR: logging.StreamHandler(self.err_buf)
-        })
-        self.mockHandleError = MagicMock()
-        self.handler.handleError = self.mockHandleError
-        self.logger = logging.getLogger('test')
-        self.logger.setLevel(logging.INFO)
-        self.logger.addHandler(self.handler)
+        }
 
     def test_output_to_shared_handler(self):
-        self.logger.info("info message")
-        self.logger.warning("warning message")
+        handler, logger = self.create_handler_and_logger(self.handlers, None)
+        logger.info("info message")
+        logger.warning("warning message")
 
         self.assertEqual(self.info_warn_buf.getvalue(), "info message\nwarning message\n")
         self.assertEqual(self.err_buf.getvalue(), "")
-        self.mockHandleError.assert_not_called()
+        self.assertEqual(self.default_buf.getvalue(), "")
+        handler.handleError.assert_not_called()
 
     def test_output_to_not_shared_handler(self):
-        self.logger.error("error message")
+        handler, logger = self.create_handler_and_logger(self.handlers, None)
+        logger.error("error message")
 
         self.assertEqual(self.info_warn_buf.getvalue(), "")
         self.assertEqual(self.err_buf.getvalue(), "error message\n")
-        self.mockHandleError.assert_not_called()
+        self.assertEqual(self.default_buf.getvalue(), "")
+        handler.handleError.assert_not_called()
 
     def test_output_to_not_set(self):
-        self.logger.critical("critical message")
+        handler, logger = self.create_handler_and_logger(self.handlers, None)
+        logger.critical("critical message")
 
         self.assertEqual(self.info_warn_buf.getvalue(), "")
         self.assertEqual(self.err_buf.getvalue(), "")
-        self.mockHandleError.assert_not_called()
+        self.assertEqual(self.default_buf.getvalue(), "")
+        handler.handleError.assert_not_called()
+
+    def test_output_to_not_set_with_default(self):
+        handler, logger = self.create_handler_and_logger(self.handlers, logging.StreamHandler(self.default_buf))
+        logger.critical("critical message")
+
+        self.assertEqual(self.info_warn_buf.getvalue(), "")
+        self.assertEqual(self.err_buf.getvalue(), "")
+        self.assertEqual(self.default_buf.getvalue(), "critical message\n")
+        handler.handleError.assert_not_called()
+
+    def create_handler_and_logger(self, handlers, default):
+        handler = PerLevelHandler(handlers, default)
+        handler.handleError = MagicMock()
+        logger = logging.getLogger('test')
+        logger.setLevel(logging.INFO)
+        logger.addHandler(handler)
+
+        return handler, logger
